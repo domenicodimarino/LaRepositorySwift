5d1115935bbc18cc0e92f6156cea779cf103fe51
Di Marino Domenico <d.dimarino8@studenti.unisa.it>
2025-06-20 09:46:47 +0200

    Initial Commit

---
+++ EsercizioSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EsercizioSwiftUI
+//
+//  Created by Domenico Di Marino on 20/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EsercizioSwiftUI/EsercizioSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsercizioSwiftUIApp.swift
+//  EsercizioSwiftUI
+//
+//  Created by Domenico Di Marino on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EsercizioSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
