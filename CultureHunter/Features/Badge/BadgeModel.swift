229c8c27e40b6d3caad2e5f9787c8cf1baf4a2d7
Giovanni Adinolfi <g.adinolfi39@studenti.unisa.it>
2025-07-18 13:06:18 +0200

    PERSISTENZA BADGE

--- CultureHunter/Features/Badge/BadgeModel.swift
+++ CultureHunter/Features/Badge/BadgeModel.swift
@@ -1,13 +1,24 @@
 import Foundation
 
-struct BadgeModel: Identifiable {
-    let id = UUID()
+struct BadgeModel: Identifiable, Codable { // <--- AGGIUNGI Codable QUI
+    let id: UUID
     let cityName: String
     var totalPOI: Int
     var certifiedPOI: Int
     var unlockedDate: Date?
-    var discoveredImageNames: [String] = []  // <-- Usa i nomi asset
+    var discoveredImageNames: [String] = []
     var cityStory: String
+
+    // MARK: - Init custom per id opzionale
+    init(id: UUID = UUID(), cityName: String, totalPOI: Int, certifiedPOI: Int, unlockedDate: Date?, discoveredImageNames: [String] = [], cityStory: String) {
+        self.id = id
+        self.cityName = cityName
+        self.totalPOI = totalPOI
+        self.certifiedPOI = certifiedPOI
+        self.unlockedDate = unlockedDate
+        self.discoveredImageNames = discoveredImageNames
+        self.cityStory = cityStory
+    }
 
     var isUnlocked: Bool {
         certifiedPOI >= totalPOI
